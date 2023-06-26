3c1619f7c470428d1e8a776f8baaa6660589c787
Vitor <84788940+VBSX@users.noreply.github.com>
2023-06-26 12:12:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,93 +4,34 @@
 ###############################################################
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
-import time
-import requests
-import os
-import subprocess
+import os, subprocess
+from modules.driver_get_whatsapp import get_whatsapp_installable_file
+from modules.whatsapp_install_powershell import whatsapp_install_in_powershell
+from modules.shutdown_whatsapp import shutdown_whatsapp
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Chrome options
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--log-level=3")  # Silences error messages from Selenium console
+def set_chrome_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--log-level=3")  # Silences error messages from Selenium console
+
+    return options
 
 # Create a new browser instance
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=set_chrome_options())
 
-try:
-    # Visit the web page
-    driver.get("https://store.rg-adguard.net/")
+get_whatsapp_installable_file(driver)
 
-    # Wait for the page to fully load
-    time.sleep(2)
-
-    # Locate the input field and send the URL
-    input_element = driver.find_element(By.ID, "url")
-    input_element.clear()
-    input_element.send_keys("https://apps.microsoft.com/store/detail/whatsapp/9NKSQGP7F2NH")
-
-    # Locate the button and click it
-    button = driver.find_element(By.XPATH, "//input[@type='button']")
-    button.click()
-
-    # Wait for the page to fully load
-    time.sleep(10)
-
-    # Get the page source
-    page_source = driver.page_source
-
-    # Parse the page source with BeautifulSoup
-    soup = BeautifulSoup(page_source, 'html.parser')
-
-    # Find the first occurrence of a .msixbundle link and download it
-    for link in soup.find_all('a'):
-        if '.msixbundle' in link.text:
-            # File URL
-            url = link['href']
-
-            # If the file to download already exists, remove it
-            filename = link.text
-            if os.path.isfile(filename):
-                os.remove(filename)
-
-            # Download the file
-            response = requests.get(url, stream=True)
-            with open(filename, 'wb') as fd:
-                for chunk in response.iter_content(chunk_size=1024):
-                    fd.write(chunk)
-
-            # If 'whatsapp.msixbundle' file already exists, remove it
-            new_filename = "whatsapp.msixbundle"
-            if os.path.isfile(new_filename):
-                os.remove(new_filename)
-
-            # Rename the downloaded file to "whatsapp.msixbundle"
-            os.rename(filename, new_filename)
-
-            break
-finally:
-    # Close the browser
-    driver.quit()
-
-try:
-    # Close the WhatsApp application
+if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "whatsapp.msixbundle")):
     try:
-        subprocess.run(['taskkill', '/F', '/IM', 'WhatsApp.exe'], check=True, stderr=subprocess.DEVNULL)  # Silences taskkill errors
+        shutdown_whatsapp()
+        whatsapp_install_in_powershell()
+        
     except subprocess.CalledProcessError as e:
-        if e.returncode != 128:
-            raise
+        print(f'Error executing command: {e}')
 
-    # Install the WhatsApp application using PowerShell
-    subprocess.run(['powershell', 'Add-AppxPackage .\\whatsapp.msixbundle'], check=True)
-
-    # Remove the "whatsapp.msixbundle" file
-    os.remove('whatsapp.msixbundle')
-
-except subprocess.CalledProcessError as e:
-    print(f'Error executing command: {e}')
-
-except OSError as e:
-    print(f'Error removing file: {e}')
+    except OSError as e:
+        print(f'Error removing file: {e}')
+else:
+    print("Whatsapp package not found")
