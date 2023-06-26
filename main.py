58a0ed96ec0bbc153dcea8565b72dc98dde4aa13
Vitor <84788940+VBSX@users.noreply.github.com>
2023-06-26 13:14:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from modules.whatsapp_install_powershell import whatsapp_install_in_powershell
 from modules.shutdown_whatsapp import shutdown_whatsapp
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 
 def set_chrome_options():
     options = webdriver.ChromeOptions()
@@ -20,18 +21,20 @@ def set_chrome_options():
 
 # Create a new browser instance
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=set_chrome_options())
+while True:
+    get_whatsapp_installable_file(driver)
 
-get_whatsapp_installable_file(driver)
-
-if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "whatsapp.msixbundle")):
-    try:
-        shutdown_whatsapp()
-        whatsapp_install_in_powershell()
-        
-    except subprocess.CalledProcessError as e:
-        print(f'Error executing command: {e}')
-
-    except OSError as e:
-        print(f'Error removing file: {e}')
-else:
-    print("Whatsapp package not found")
+    if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "whatsapp.msixbundle")):
+        try:
+            shutdown_whatsapp()
+            whatsapp_install_in_powershell()
+            break
+        except subprocess.CalledProcessError as e:
+            print(f'Error executing command: {e}')
+            sleep(10)
+        except OSError as e:
+            print(f'Error removing file: {e}')
+            sleep(10)
+    else:
+        print("Whatsapp package not found")
+        sleep(10)
